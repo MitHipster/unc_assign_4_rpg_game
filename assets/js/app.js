b6c3f8bda1aa332113258580963d899fc5a2949c
Tim Acker <tjacker31@gmail.com>
2017-06-10 17:01:08 -0400

    Game Object
    Started constructing game object and added animate function to slide hero characters to the middle of the screen when selected.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,49 @@
 /*jslint esversion: 6, browser: true*/
 /*global window, console, $, jQuery, alert*/
 
+let rpg = {
+  charSelect: false,
+  oppSelect: false,
+  character: {
+    captAmerica: {
+      healthPts: 150,
+      attackPower: 5,
+      counterPower: 20,
+      team: 'blue'
+    },
+    falcon: {
+      healthPts: 120,
+      attackPower: 3,
+      counterPower: 15,
+      team: 'blue'
+    },
+    ironMan: {
+      healthPts: 170,
+      attackPower: 6,
+      counterPower: 25,
+      team: 'red'
+    },
+    blackWidow: {
+      healthPts: 110,
+      attackPower: 4,
+      counterPower: 20,
+      team: 'red'
+    }
+  }
+};
+
+$('.hero-container').click( function() {
+  if ($(this).hasClass('team-blue')) {
+    animateChar($(this), 'left');
+  } else {
+    animateChar($(this), 'right');
+  }
+});
+
+let animateChar = function (element, side) {
+  let pos = '25%';
+  element.animate({
+    top: pos,
+    [side]: pos
+  }, 'slow');
+};
