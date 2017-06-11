1cb785d3d19fbf59c7312a75bf3aaab3a11887e3
Tim Acker <tjacker31@gmail.com>
2017-06-11 16:01:46 -0400

    Attack Button
    Added an attack button to initiate the simulated fight and revised the click hero function to only allow player and opponent selections - one from each team.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,15 @@
 /*global window, console, $, jQuery, alert*/
 
 const $heroes = $('.hero-container');
+const $btnText = $('#attack-btn span');
+const $message = $('#message');
 const hpClass = ".hp-value";
 
 let rpg = {
-  charSelect: false,
-  oppSelect: false,
+  charSelect: "",
+  charTeam: "",
+  oppSelect: "",
+  oppTeam: "",
   characters: {
     captainAmerica: {
       healthPts: 150,
@@ -33,29 +37,52 @@ let rpg = {
       team: 'red'
     }
   },
-  instructIndex: 0,
-  instructSet: ["Click on a hero to be your player."]
+  messageIndex: 0,
+  messages: ["Click a hero to be your player.", "Select an opponent to battle from the other team.", "Press attack to battle your opponent."],
+  btnText: ["Select", "Versus", "Attack"]
 };
 
 let setupGame = function () {
   $.each(rpg.characters, function(character, obj) {
-    console.log($('main').find(`[data-hero="${character}"] ${hpClass}`).text(obj.healthPts + "pts"));
+    $('main').find(`[data-hero="${character}"] ${hpClass}`).text(obj.healthPts);
   });
+  $message.text(rpg.messages[0]);
+  rpg.charSelect = "";
+  rpg.oppSelect = "";
 };
 
 $heroes.click( function() {
-  if ($(this).hasClass('team-blue')) {
-    animateChar($(this), 'left');
-  } else {
-    animateChar($(this), 'right');
+  let $this = $(this);
+  let charSide = function () {
+    if ($this.data("team") === 'blue') {
+      animateChar($this, 'left');
+    } else {
+      animateChar($this, 'right');
+    }
+  };
+  let assign = function (hero, team) {
+    rpg[hero] = $this.data("hero");
+    rpg[team] =  $this.data("team");
+  };
+  if (rpg.charSelect === "") {
+    assign('charSelect', 'charTeam');
+    charSide();
+    $message.text(rpg.messages[1]);
+    $btnText.text(rpg.btnText[1]);
+  } else if (rpg.oppSelect === "" && $this.data("team") !== rpg.charTeam) {
+    assign('oppSelect', 'oppTeam');
+    charSide();
+    $message.text(rpg.messages[2]);
+    $btnText.text(rpg.btnText[2]).css('cursor', 'pointer').prop('disabled', false);
   }
 });
 
 let animateChar = function (element, side) {
-  let pos = '25%';
+  let topPos = '28%';
+  let sidePos = '24%';
   element.animate({
-    top: pos,
-    [side]: pos
+    top: topPos,
+    [side]: sidePos
   }, 'slow').css('bottom', 'auto');
 };
 
